0d84eda1db4fa9be3c6d8cd2fcaecef45e08ba83
Jake C <jake@asystemdesign.com>
2023-05-24 12:08:40 -0400

    transition from the bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -110,40 +110,66 @@ import { RouterOutlet } from '@angular/router';
   //     ])
   //   ])
   // ]
+  // animations: [
+  //   trigger('routeAnimations', [
+  //     transition('* => *', [
+  //       style({ position: 'relative' }),
+  //       query(
+  //         ':enter, :leave',
+  //         [
+  //           style({
+  //             position: 'absolute',
+  //             top: 0,
+  //             left: 0,
+  //             width: '100%',
+  //             height: '100%',
+  //           }),
+  //         ],
+  //         { optional: true }
+  //       ),
+  //       query(':enter', [style({ transform: 'translateX(100%)' })], {
+  //         optional: true,
+  //       }),
+  //       group([
+  //         query(
+  //           ':leave',
+  //           [animate('300ms ease-out', style({ transform: 'translateX(-100%)' }))],
+  //           { optional: true }
+  //         ),
+  //         query(
+  //           ':enter',
+  //           [animate('300ms ease-out', style({ transform: 'translateX(0)' }))],
+  //           { optional: true }
+  //         ),
+  //       ]),
+  //     ]),
+  //   ]),
+  // ]
+  // animations: [
+  //   trigger('routeAnimations', [
+  //     transition('* <=> *', [
+  //       query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
+  //       query(':enter', style({ transform: 'translateX(100%)' }), { optional: true }),
+  //       group([
+  //         query(':leave', [animate('300ms ease-out', style({ transform: 'translateX(-100%)' }))], { optional: true }),
+  //         query(':enter', [animate('300ms ease-out', style({ transform: 'translateX(0)' }))], { optional: true })
+  //       ])
+  //     ])
+  //   ])
+  // ]
   animations: [
     trigger('routeAnimations', [
-      transition('* => *', [
-        style({ position: 'relative' }),
-        query(
-          ':enter, :leave',
-          [
-            style({
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-            }),
-          ],
-          { optional: true }
-        ),
-        query(':enter', [style({ transform: 'translateX(100%)' })], {
-          optional: true,
-        }),
-        group([
-          query(
-            ':leave',
-            [animate('300ms ease-out', style({ transform: 'translateX(-100%)' }))],
-            { optional: true }
-          ),
-          query(
-            ':enter',
-            [animate('300ms ease-out', style({ transform: 'translateX(0)' }))],
-            { optional: true }
-          ),
-        ]),
-      ]),
-    ]),
+      transition('* <=> *', [
+        query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
+        query(':enter', [
+          style({ transform: 'translateY(100%)' }),
+          animate('300ms ease-out', style({ transform: 'translateY(0%)' }))
+        ], { optional: true }),
+        query(':leave', [
+          animate('300ms ease-out', style({ transform: 'translateY(100%)' }))
+        ], { optional: true }),
+      ])
+    ])
   ]
 })
 export class AppComponent {
